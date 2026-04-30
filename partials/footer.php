0bf981d84e19aeb035ef32f3385aa80b0532ee52
baogroup <165123236+baogroup@users.noreply.github.com>
2026-04-30 21:01:42 +0300

    Add footer partial

---
+++ partials/footer.php
@@ -0,0 +1,21 @@
+</main>
+<footer class="site-footer">
+    <div class="container footer-grid">
+        <div>
+            <strong><?= e($config['company_name']) ?></strong>
+            <p>Įm.k. <?= e($config['company_code']) ?><br>PVM <?= e($config['vat_code']) ?></p>
+        </div>
+        <div>
+            <strong><?= e(tr($t, 'section.contacts')) ?></strong>
+            <p><?= e($config['address']) ?><br><a href="tel:<?= e($config['phone_href']) ?>"><?= e($config['phone']) ?></a><br><a href="mailto:<?= e($config['email']) ?>"><?= e($config['email']) ?></a></p>
+        </div>
+        <div>
+            <strong>Informacija</strong>
+            <p><a href="<?= e(url_for('privacy', $lang)) ?>"><?= e(tr($t, 'footer.privacy')) ?></a><br><a href="<?= e(url_for('cookies', $lang)) ?>"><?= e(tr($t, 'footer.cookies')) ?></a></p>
+        </div>
+    </div>
+</footer>
+<?php require __DIR__ . '/cookie-banner.php'; ?>
+<script src="assets/js/app.js"></script>
+</body>
+</html>
