49f730f8f2a9cb1d6cb7806134141355c001e40d
baogroup <165123236+baogroup@users.noreply.github.com>
2026-04-30 21:02:09 +0300

    Add initial blog post data

---
+++ data/blog-posts.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+    [
+        'title' => [
+            'lt' => 'Kada reikia skiepyti šunį?',
+            'en' => 'When should a dog be vaccinated?',
+            'ru' => 'Когда нужно вакцинировать собаку?',
+        ],
+        'excerpt' => [
+            'lt' => 'Trumpas priminimas apie profilaktinę vakcinaciją ir kodėl jos nereikėtų atidėti.',
+            'en' => 'A short reminder about preventive vaccination and why it should not be postponed.',
+            'ru' => 'Короткое напоминание о профилактической вакцинации и почему её не стоит откладывать.',
+        ],
+    ],
+    [
+        'title' => [
+            'lt' => 'Ką daryti radus erkę?',
+            'en' => 'What to do if you find a tick?',
+            'ru' => 'Что делать, если вы нашли клеща?',
+        ],
+        'excerpt' => [
+            'lt' => 'Pirmieji žingsniai ir situacijos, kai verta kreiptis į veterinarijos gydytoją.',
+            'en' => 'First steps and situations when it is worth contacting a veterinarian.',
+            'ru' => 'Первые шаги и ситуации, когда лучше обратиться к ветеринарному врачу.',
+        ],
+    ],
+    [
+        'title' => [
+            'lt' => 'Kodėl katė nustojo valgyti?',
+            'en' => 'Why has a cat stopped eating?',
+            'ru' => 'Почему кошка перестала есть?',
+        ],
+        'excerpt' => [
+            'lt' => 'Apetito praradimas gali būti laikinas, bet kai kuriais atvejais reikia nelaukti.',
+            'en' => 'Loss of appetite can be temporary, but in some cases it should not be ignored.',
+            'ru' => 'Потеря аппетита может быть временной, но иногда ждать нельзя.',
+        ],
+    ],
+];
