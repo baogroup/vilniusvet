3ff5cae42dccc7abbde50d7648420497f0474e35
baogroup <165123236+baogroup@users.noreply.github.com>
2026-04-30 22:05:04 +0300

    Render real articles on blog page

--- pages/blog.php
+++ pages/blog.php
@@ -1,3 +1,8 @@
+<?php
+$articles = require __DIR__ . '/../data/articles.php';
+$blogBase = url_for('blog', $lang);
+?>
+
 <section class="page-hero">
     <div class="container narrow">
         <h1><?= e(tr($t, 'nav.blog')) ?></h1>
@@ -7,11 +12,18 @@
 
 <section class="section">
     <div class="container blog-grid">
-        <?php foreach ($posts as $post): ?>
+        <?php foreach ($articles as $article): ?>
+            <?php
+            $articleSlug = $article['slugs'][$lang] ?? $article['slugs']['lt'];
+            $articleTitle = $article['title'][$lang] ?? $article['title']['lt'];
+            $articleExcerpt = $article['excerpt'][$lang] ?? $article['excerpt']['lt'];
+            ?>
             <article class="card blog-card">
                 <span class="eyebrow">Veterinarijos patarimai</span>
-                <h2><?= e($post['title'][$lang] ?? $post['title']['lt']) ?></h2>
-                <p><?= e($post['excerpt'][$lang] ?? $post['excerpt']['lt']) ?></p>
+                <h2><a href="<?= e($blogBase . $articleSlug . '/') ?>"><?= e($articleTitle) ?></a></h2>
+                <p class="blog-date"><?= e($article['date']) ?></p>
+                <p><?= e($articleExcerpt) ?></p>
+                <a class="text-link" href="<?= e($blogBase . $articleSlug . '/') ?>"><?= e(tr($t, 'blog.read_more')) ?> →</a>
             </article>
         <?php endforeach; ?>
     </div>
